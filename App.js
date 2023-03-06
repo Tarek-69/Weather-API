7124a9d2256d35c64da458fcce51e3f5a645de69
Tarek <t.taguine@it-students.fr>
2023-03-06 10:18:14 +0100

    Add Prevision and color

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default function App() {
 
   const getWeather = async (lat, long) => {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${myAPI}&units=metric&lang={fr}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${myAPI}&units=metric&lang=fr`
     );
     const data = await response.json();
     setCityData(data);
@@ -30,15 +30,12 @@ export default function App() {
   {
     /*Configuration des données pour les prévisions*/
   }
-  const getPrevisionWeather = async (lat, long) => {
+  const getPrevisionWeather = async (lat, lon) => {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${myAPI}&units=metric&lang={fr}`
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${myAPI}&units=metric&lang=fr`
     );
-
     const datas = await response.json();
-    setPrevision(datas);
-    console.log(datas);
-    console.log(prevision);
+    setPrevision(datas.list);
   };
 
   useEffect(() => {
@@ -57,34 +54,114 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      {/* Affichage du nom de la ville */}
       {cityData ? (
-        <Text>{cityData.name}</Text>
-      ) : (
-        <ActivityIndicator size="large" color="#00ff00" />
-      )}
-      {/* Affichage du lieu localisé*/}
-      {cityData ? (
-        <Text>Le lieu est : {cityData.sys.country}</Text>
-      ) : (
-        <ActivityIndicator size="large" color="#00ff00" />
-      )}
-      {/* Affichage de la température*/}
-      {cityData ? (
-        <Text>La température est de : {cityData.main.temp} °C</Text>
-      ) : (
-        <ActivityIndicator size="large" color="#00ff00" />
-      )}
-      {weatherIcon ? (
-        <Image
-          source={{
-            uri: `https://openweathermap.org/img/wn/${weatherIcon}.png`,
+        <View
+          style={{
+            flex: 0.2,
+            alignItems: "center",
+            justifyContent: "space-around",
+            width: "100%",
+            paddingTop: 100,
           }}
-          style={{ width: 100, height: 100 }}
-        />
+        >
+          <Text
+            style={{
+              fontSize: 32,
+            }}
+          >
+            {cityData.name}
+          </Text>
+          <Text style={{ fontSize: 48, paddingTop: 25 }}>
+            {Math.floor(cityData.main.temp)} °C
+          </Text>
+        </View>
       ) : (
         <ActivityIndicator size="large" color="#00ff00" />
       )}
+
+      {weatherIcon ? (
+        <View
+          style={{
+            flex: 0.5,
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <Image
+            source={{
+              uri: `https://openweathermap.org/img/wn/${weatherIcon}@4x.png`,
+            }}
+            style={{ width: 200, height: 200 }}
+          />
+          <Text style={{ fontSize: 48, paddingTop: 25 }}>
+            {cityData.weather[0].description}
+          </Text>
+        </View>
+      ) : (
+        <ActivityIndicator size="large" color="#00ff00" />
+      )}
+
       {/* Affichage de la température*/}
+
+      {prevision ? (
+        <View style={{ flex: 0.3 }}>
+          {console.log(prevision)}
+
+          <FlatList
+            data={prevision}
+            keyExtractor={(item) => item.dt.toString()}
+            horizontal={true}
+            renderItem={({ item }) => (
+              <>
+                <View
+                  style={{
+                    marginHorizontal: 5,
+                    paddingHorizontal: 20,
+                    paddingVertical: 10,
+                    flexDirection: "column",
+                    backgroundColor: "lightgrey",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 18,
+                      fontWeight: "bold",
+                      textTransform: "capitalize",
+                    }}
+                  >
+                    {new Date(item.dt_txt).toLocaleDateString("fr-FR", {
+                      weekday: "short",
+                      day: "numeric",
+                      month: "short",
+                      hour: "numeric",
+                    })}
+                  </Text>
+                  <Image
+                    source={{
+                      uri: `http://openweathermap.org/img/wn/${item.weather[0].icon}@4x.png`,
+                    }}
+                    style={{ height: 150, width: 150 }}
+                  ></Image>
+                  <Text
+                    style={{
+                      fontSize: 24,
+                      color: "#222222",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {Math.floor(item.main.temp)} <Text>°C</Text>
+                  </Text>
+                </View>
+              </>
+            )}
+          />
+        </View>
+      ) : (
+        <ActivityIndicator size="large" color="#00ff00" />
+      )}
 
       <StatusBar style="auto" />
     </View>
